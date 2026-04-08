35ed11b4e48dcc4b15605a019aeb0d5eae329524
Bekzat-top4ik <bekzatasanov2009@gmail.com>
2026-04-08 22:11:12 +0600

    add gunicorn and uvicorn for render

--- bookings/utils.py
+++ bookings/utils.py
@@ -6,6 +6,9 @@ def send_telegram_message(text: str):
     token = getattr(settings, "TELEGRAM_BOT_TOKEN", "")
     chat_id = getattr(settings, "TELEGRAM_CHAT_ID", "")
 
+    print("TOKEN EXISTS:", bool(token))
+    print("CHAT ID:", chat_id)
+
     if not token:
         print("TELEGRAM_BOT_TOKEN not found")
         return
